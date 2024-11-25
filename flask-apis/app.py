86efc0f5242827f29cae267764f712f6ea874c24
unknown <wintun.hlaing@outlook.com>
2024-11-25 13:28:31 +0630

    resources

--- flask-apis/app.py
+++ flask-apis/app.py
@@ -2,12 +2,14 @@ from server import create_app
 
 #import blue print
 from server.routes.server_test import api_test_bp
+from server.routes.auth.auth_test import auth_test_bp
 
 #create app instance
 app = create_app()
 
 #Blue print register
 app.register_blueprint(api_test_bp)
+app.register_blueprint(auth_test_bp)
 
 if __name__ == "__main__":
     #add host as 0.0.0.0 to be accessible from outside when running as a #container
---
+++ flask-apis/server/resources/apis.py
@@ -0,0 +1,9 @@
+
+#api end-points
+AUTH_API_LINK_TEST = "/api/v1/auth/test"
+AUTHORIZED_TEST ="/api/v1/test-authorize"
+USER_REGISER="/api/v1/auth/register"
+USER_LOGIN="/api/v1/auth/login"
+USERS="/api/v1/users"
+USER="/api/v1/user"
+PRODUCT="/api/v1/products"
