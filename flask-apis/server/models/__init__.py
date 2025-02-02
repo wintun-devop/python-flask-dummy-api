0268e61ce54ad652c30feb6d7e498f726e012b9b
wintun-devop <wintun.hlaing@outlook.com>
2025-02-02 17:56:04 +0630

    update model

--- flask-apis/server/models/__init__.py
+++ flask-apis/server/models/__init__.py
@@ -6,9 +6,10 @@ db = SQLAlchemy()
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.String(), primary_key=True, default=str(uuid.uuid4()))
-    name = db.Column(db.String(150))
-    customId=db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(), nullable=False)
+    username =  db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    profile = db.Column(db.string(255),nullable=True,default=str("profile.png"))
+    permission = db.Column(db.string,nullable=True,default=str("customer"))
     createdAt = db.Column(db.DateTime,default=datetime.datetime.utcnow())
     updateAt = db.Column(db.DateTime,onupdate=datetime.datetime.utcnow())
